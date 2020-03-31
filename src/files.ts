48b9fadc5fc6324414e156535919db5a60f38bb9
sree chandan <sreechandan98@gmail.com>
2020-03-31 23:26:32 +0530

    improved files.ts

--- src/files.ts
+++ src/files.ts
@@ -2,33 +2,46 @@ import * as Structs from "./structs";
 import * as fs from "fs";
 import * as path from "path";
 
-export async function initDatabase(dataBasePath: string): Promise<void> {
-  await fs.mkdir(dataBasePath, { recursive: true }, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      fs.mkdirSync(path.join(dataBasePath, "Entities"), { recursive: true });
-      fs.mkdirSync(path.join(dataBasePath, "Collections"), { recursive: true });
-      fs.mkdirSync(path.join(dataBasePath, "Relations"), { recursive: true });
-      return new Promise((resolve) => {
+export async function initDatabase(
+  dataBasePath: string,
+  mode: "init" | "reset" = "init"
+): Promise<void> {
+  function init(): void {
+    fs.mkdirSync(dataBasePath, { recursive: true });
+    fs.mkdirSync(path.join(dataBasePath, "Entities"), { recursive: true });
+    fs.mkdirSync(path.join(dataBasePath, "Collections"), { recursive: true });
+    fs.mkdirSync(path.join(dataBasePath, "Relations"), { recursive: true });
+  }
+  switch (mode) {
+    case "reset":
+      // remove the database folder completely
+      if (fs.existsSync(dataBasePath)) {
+        fs.rmdirSync(dataBasePath, { recursive: true });
+      }
+      init();
+      return new Promise((resolve, reject) => {
+        console.log("reset done");
         resolve();
       });
-    }
-  });
+
+      break;
+    case "init":
+      // check database folder integrity
+      if (!fs.existsSync(dataBasePath)) {
+        init();
+      }
+      break;
+  }
 }
 
 export function writeEntity(
   entity: Structs.Entity,
   dataBasePath: string
 ): void {
-  fs.writeFile(
+  fs.writeFileSync(
     path.resolve(path.join(dataBasePath, "Entities", entity.ID + ".entity")),
     JSON.stringify(entity),
-    { flag: "w+" },
-    (err) => {
-      if (err) throw err;
-      console.log("The file has been saved!");
-    }
+    { flag: "w+" }
   );
 }
 
